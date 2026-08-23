956efc551c1074a7c8baa36fcadcdb28a02ead75
unstoppalezzz <unstoppalezzz@protonmail.com>
2026-08-23 13:57:32 -0400

    Delete mapping version/src/client/java/com/github/zly2006/reden directory

--- mapping version/src/client/java/com/github/zly2006/reden/gui/componments/UpdatableTextBox.kt
+++
@@ -1,9 +0,0 @@
-package com.github.unstoppalezzz.reden.gui.componments
-
-import io.wispforest.owo.ui.core.Insets
-
-/**
- * To keep our text in the center of the textbox.
- * Don't ask me why top is greater than bottom for 1px, ask Mojang.
- */
-private val fuckMojangMargins = Insets.of(2, 1, 2, 2)
--- mapping version/src/client/java/com/github/zly2006/reden/mixin/client/otherMods/owo/ITextureComponent.java
+++
@@ -1,4 +0,0 @@
-package com.github.unstoppalezzz.reden.mixin.client.otherMods.owo;
-
-public interface ITextureComponent {
-}
